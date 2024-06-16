5b0eb45cf05d7464333eee6e3539e30fcaeeda55
1GabrielC <gabriel19cassiano84@gmail.com>
2024-06-16 18:03:12 -0300

    16/06/24

---
+++ JS/aula1.js
@@ -0,0 +1,15 @@
+let nome,nota1,nota2,media;
+nome = prompt("Digite o seu nome");
+nota1 = parseFloat(prompt ("Digite a primeira nota"));
+nota2 = parseFloat(prompt ("Digite a segunda nota"));
+media = (nota1+nota2)/2;
+
+
+if(media>=7){
+    resposta = "Aprovado"
+}else{
+    resposta = "Reprovado"
+}
+
+
+alert(`${nome}, sua média do aluno é ${media.toFixed(1)} você está ${resposta}`);
